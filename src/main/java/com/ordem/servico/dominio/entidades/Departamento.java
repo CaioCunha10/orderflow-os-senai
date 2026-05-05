378b09ca715593530ed4aca3fb705788836064f9
Gabriel <muller.gabriel48@yahoo.com>
2026-05-05 18:27:47 -0300

    5 - criaçao da entidade Departamento

---
+++ src/main/java/com/ordem/servico/dominio/entidades/Departamento.java
@@ -0,0 +1,39 @@
+package com.ordem.servico.dominio.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Departamento() {}
+
+    public Departamento(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
