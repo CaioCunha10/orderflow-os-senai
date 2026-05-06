073ecee27ff2dcf1cd6dd45722e44eafdb47c6f8
redgame94 <kaualarasoares@gmail.com>
2026-05-06 16:48:17 -0300

    Correção do POM

--- src/main/java/com/ordem/servico/dominio/entidades/Departamento.java
+++ src/main/java/com/ordem/servico/dominio/entidades/Departamento.java
@@ -1,6 +1,7 @@
 package com.ordem.servico.dominio.entidades;
 
 import jakarta.persistence.Entity;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
