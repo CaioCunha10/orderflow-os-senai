667859af99648c8f856f7d7c0b953a36d8a10a7d
redgame94 <kaualarasoares@gmail.com>
2026-05-06 15:19:52 -0300

    4 - criar entidade OrdemServico

--- src/main/java/com/ordem/servico/dominio/entidades/OrdemServico.java
+++ src/main/java/com/ordem/servico/dominio/entidades/OrdemServico.java
@@ -5,12 +5,12 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "ordem_de_servico")
-
 public class OrdemServico {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private LocalDate dataAbertura;
     private LocalDate dataInicial;
     private LocalDate dataFinalizacao;
@@ -18,31 +18,46 @@ public class OrdemServico {
 
     @Enumerated(EnumType.STRING)
     private StatusOS status;
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
 
-    public LocalDate getDataAbertura() 
-    { return dataAbertura; }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public void setDataAbertura(LocalDate dataAbertura) { this.dataAbertura = dataAbertura; }
+    public LocalDate getDataAbertura() {
+        return dataAbertura;
+    }
+    public void setDataAbertura(LocalDate dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
 
-    public LocalDate getDataInicio() 
-    { return dataInicial; }
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+    public void setDataInicial(LocalDate dataInicial) {
+        this.dataInicial = dataInicial;
+    }
 
-    public void setDataInicio(LocalDate dataInicial) { this.dataInicial = dataInicial; }
+    public LocalDate getDataFinalizacao() {
+        return dataFinalizacao;
+    }
+    public void setDataFinalizacao(LocalDate dataFinalizacao) {
+        this.dataFinalizacao = dataFinalizacao;
+    }
 
-    public LocalDate getDataFinalizacao() 
-    { return dataFinalizacao; }
+    public String getDescricaoTecnica() {
+        return descricaoTecnica;
+    }
+    public void setDescricaoTecnica(String descricaoTecnica) {
+        this.descricaoTecnica = descricaoTecnica;
+    }
 
-    public void setDataFinalizacao(LocalDate dataFinalizacao) { this.dataFinalizacao = dataFinalizacao; }
-
-    public String getDescricaoTecnica() 
-    { return descricaoTecnica; }
-
-    public void setDescricaoTecnica(String descricaoTecnica) { this.descricaoTecnica = descricaoTecnica; }
-
-    public StatusOS getStatus() 
-    { return status; }
-
-    public void setStatus(StatusOS status) { this.status = status; }
+    public StatusOS getStatus() {
+        return status;
+    }
+    public void setStatus(StatusOS status) {
+        this.status = status;
+    }
 }
