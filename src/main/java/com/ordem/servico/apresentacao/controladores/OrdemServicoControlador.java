ecff83ac93aeb6c8890d7ba31b3e634b4d541d35
redgame94 <kaualarasoares@gmail.com>
2026-05-07 15:31:12 -0300

    17 - criar endpoint GET /ordens

--- src/main/java/com/ordem/servico/apresentacao/controladores/OrdemServicoControlador.java
+++ src/main/java/com/ordem/servico/apresentacao/controladores/OrdemServicoControlador.java
@@ -1,4 +1,27 @@
 package com.ordem.servico.apresentacao.controladores;
 
+
+import java.util.List;
+
+import com.ordem.servico.dominio.entidades.OrdemServico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ordem.servico.infraestrutura.repositorios.OrdemServicoRepositorio;
+
+@RestController
+@RequestMapping("/ordens")
 public class OrdemServicoControlador {
-}
+	
+	@Autowired
+	private OrdemServicoRepositorio repositorio;
+	
+	@GetMapping
+	public List<OrdemServico> ListarTodas(){
+		return repositorio.findAll();
+	}
+	
