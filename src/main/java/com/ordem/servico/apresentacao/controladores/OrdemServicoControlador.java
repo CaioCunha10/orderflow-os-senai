6ef31e63efa7bbebdf1c7ba063f6bc8479fc5108
Pedro Oliva <pedri@LeN1wE2c4H.LeN1wE2c4H>
2026-05-07 15:18:29 -0300

    16 - criar endpoint POST ordens

--- src/main/java/com/ordem/servico/apresentacao/controladores/OrdemServicoControlador.java
+++ src/main/java/com/ordem/servico/apresentacao/controladores/OrdemServicoControlador.java
@@ -1,4 +1,25 @@
 package com.ordem.servico.apresentacao.controladores;
 
+import com.ordem.servico.aplicacao.services.OrdemServicoService;
+import com.ordem.servico.apresentacao.dtos.OrdemServicoCreateDTO;
+import com.ordem.servico.apresentacao.dtos.OrdemServicoDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ordens")
 public class OrdemServicoControlador {
+
+    private final OrdemServicoService ordemServicoService;
+
+    public OrdemServicoControlador(OrdemServicoService ordemServicoService) {
+        this.ordemServicoService = ordemServicoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrdemServicoDTO> criar(@RequestBody OrdemServicoCreateDTO dto) {
+        OrdemServicoDTO ordemCriada = ordemServicoService.criar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ordemCriada);
+    }
 }
