07f4fb4c1cb30a7954779cc596aacd45a2c8f153
Julius Lemos <juliuslemos1@gmail.com>
2026-05-07 14:53:25 -0300

    12 - implementar regra de criacao de OS

--- src/main/java/com/ordem/servico/OrdemServicoApplication.java
+++ src/main/java/com/ordem/servico/OrdemServicoApplication.java
@@ -2,13 +2,12 @@ package com.ordem.servico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class OrdemServicoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrdemServicoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrdemServicoApplication.class, args);
+    }
 
 }
