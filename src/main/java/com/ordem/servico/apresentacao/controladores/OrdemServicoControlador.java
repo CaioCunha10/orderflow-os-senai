08fb4a3dab39eae4e5805ff5ea440ec90cfa11e6
Franklin1709 <franklinsouzalima.com>
2026-05-08 12:30:27 -0300

    endpoint GET ordens

--- src/main/java/com/ordem/servico/apresentacao/controladores/OrdemServicoControlador.java
+++ src/main/java/com/ordem/servico/apresentacao/controladores/OrdemServicoControlador.java
@@ -1,4 +1,24 @@
 package com.ordem.servico.apresentacao.controladores;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ordem.servico.dominio.entidades.OrdemServico;
+import com.ordem.servico.infraestrutura.repositorios.OrdemServicoRepositorio;
+
+@RestController
+@RequestMapping("/ordens")
 public class OrdemServicoControlador {
+
+    @Autowired
+    private OrdemServicoRepositorio repositorio;
+
+    @GetMapping
+    public List<OrdemServico> listarOrdens() {
+        return repositorio.findAll();
+    }
 }
